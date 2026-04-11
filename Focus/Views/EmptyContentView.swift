c99dd8d9195a71b50b369280e730aa01e7b4740f
Dan Berry <berrdaniel@gmail.com>
2026-04-11 17:59:11 -0500

    Add a custom symbol for no merged PRs, and update EmptyContentView to allow for custom symbols

--- Focus/Views/EmptyContentView.swift
+++ Focus/Views/EmptyContentView.swift
@@ -5,10 +5,18 @@ import SwiftUI
 struct EmptyContentView: View {
     private let title: String
     private let systemImage: String
+    private let name: String?
 
     init(_ title: String, systemImage: String) {
         self.title = title
         self.systemImage = systemImage
+        self.name = nil
+    }
+    
+    init(_ title: String, named: String) {
+        self.title = title
+        self.systemImage = ""
+        self.name = named
     }
 
     var body: some View {
@@ -16,11 +24,21 @@ struct EmptyContentView: View {
             Label {
                 Text(title)
             } icon: {
-                Image(systemName: systemImage)
+                image
                     .symbolRenderingMode(.palette)
                     .foregroundStyle(.accentedRed.gradient, .accentedGray.gradient)
             }
         }
         .listRowSeparator(.hidden)
     }
+    
+    var image: Image {
+        if let name {
+            Image(name)
+        }
+        else {
+            Image(systemName: systemImage)
+        }
+    }
+    
 }
